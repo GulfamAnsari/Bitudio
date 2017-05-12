3a95c41c988194e56629dfd1ee3884a2f71ee8e8
sarthak subramanian <sarthak@crindo.com>
2017-05-12 18:08:08 +0530

    bouncing and box edited

--- jQuery.js
+++ jQuery.js
@@ -6,3 +6,19 @@ function fun2()
 {
 	document.getElementById("b").style.display="none";
 }
+function fun3()
+{
+	document.getElementById("b1").style.display="none";
+}
+function fun4()
+{
+	document.getElementById("b2").style.display="none";
+}
+function fun5()
+{
+	document.getElementById("b3").style.display="none";
+}
+function fun6()
+{
+	document.getElementById("b4").style.display="none";
+}
